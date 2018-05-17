de801780e4c9d49291e3f92d64e70462336863df
keith <keithmaccuish5@gmail.com>
2018-05-17 04:22:59 -0600

    Memed
    If no dice are rolled and is only used to add a number be mean and send a random amount.

--- src/main/java/com/tree/slamJamBotV2/DnDCommands/DnD5eCommands.java
+++ src/main/java/com/tree/slamJamBotV2/DnDCommands/DnD5eCommands.java
@@ -78,6 +78,8 @@ public class DnD5eCommands {
 
 			}
 		}
+
+
         if(message.startsWith("roll")){
 			roll(channel,message);
 		}
@@ -85,41 +87,46 @@ public class DnD5eCommands {
 
 
 		if(message.startsWith("give me a random character!")){
-			RandomCharacter rc = new RandomCharacter();
-			EmbedBuilder embedBuilder = new EmbedBuilder();
-			embedBuilder.withTitle("Your random character!");
-			embedBuilder.withFooterText("This character was randomly generatored using Xanathar's Guide To Everything.");
-			embedBuilder.appendField("Age",rc.getAge(),false);
-			embedBuilder.appendField("Race & Class", rc.getRace() + "\n" + rc.getPClass(),false);
-			String abilityScores = "```" +
-					"Str:"+rc.getStr()+
-					"\nDex:"+rc.getDex()+
-					"\nCon:"+rc.getCon()+
-					"\nInt:"+rc.getInt()+
-					"\nWis:"+rc.getWis()+
-					"\nCha:"+rc.getCha()+"```";
-			embedBuilder.appendField("Why are you a "+rc.getPClass()+"?",rc.getWhyclass(),false);
-			embedBuilder.appendField("Ability Scores",abilityScores,false);
-			StringBuilder sb = new StringBuilder();
-			sb.append("```");
-			for (int i = 0; i < rc.getBackground().length - 1; i++) {
-				sb.append(rc.getBackground()[i]).append("\n");
-			}
-			sb.append("```");
-			embedBuilder.appendField("Background - " + rc.getBackground()[0],sb.toString(),false);
-			int z = 1;
-			if(rc.getLifeEvents().length == 1){
-				embedBuilder.appendField("Life event #1",rc.getLifeEvents()[0],false);
-			}else {
-				for (int i = 0; i < rc.getLifeEvents().length - 1; i++) {
-					embedBuilder.appendField("Life event #" + z, rc.getLifeEvents()[i], false);
-					z++;
-				}
-			}
-			SlamUtils.sendEmbed(channel,embedBuilder.build());
+			randomCharacter(channel);
+
 		}
 
     }
+
+	private void randomCharacter(IChannel channel) {
+		RandomCharacter rc = new RandomCharacter();
+		EmbedBuilder embedBuilder = new EmbedBuilder();
+		embedBuilder.withTitle("Your random character!");
+		embedBuilder.withFooterText("This character was randomly generatored using Xanathar's Guide To Everything.");
+		embedBuilder.appendField("Age",rc.getAge(),false);
+		embedBuilder.appendField("Race & Class", rc.getRace() + "\n" + rc.getPClass(),false);
+		String abilityScores = "```" +
+				"Str:"+rc.getStr()+
+				"\nDex:"+rc.getDex()+
+				"\nCon:"+rc.getCon()+
+				"\nInt:"+rc.getInt()+
+				"\nWis:"+rc.getWis()+
+				"\nCha:"+rc.getCha()+"```";
+		embedBuilder.appendField("Why are you a "+rc.getPClass()+"?",rc.getWhyclass(),false);
+		embedBuilder.appendField("Ability Scores",abilityScores,false);
+		StringBuilder sb = new StringBuilder();
+		sb.append("```");
+		for (int i = 0; i < rc.getBackground().length - 1; i++) {
+			sb.append(rc.getBackground()[i]).append("\n");
+		}
+		sb.append("```");
+		embedBuilder.appendField("Background - " + rc.getBackground()[0],sb.toString(),false);
+		int z = 1;
+		if(rc.getLifeEvents().length == 1){
+			embedBuilder.appendField("Life event #1",rc.getLifeEvents()[0],false);
+		}else {
+			for (int i = 0; i < rc.getLifeEvents().length - 1; i++) {
+				embedBuilder.appendField("Life event #" + z, rc.getLifeEvents()[i], false);
+				z++;
+			}
+		}
+		SlamUtils.sendEmbed(channel,embedBuilder.build());
+	}
 
 	private void roll(IChannel channel, String message) {
 		StringBuilder stringBuilder;
@@ -128,18 +135,23 @@ public class DnD5eCommands {
 		ArrayList<ArrayList<Integer>> dicerolled = new ArrayList<>();
 		//removing "roll"
 		rollCommand.remove(0);
-
+		boolean Meme = true;
 		//int to know were i'm at in the dicerolled array list
 		int x = 0;
 		for (int i = 0; i < rollCommand.size(); i++) {
+			String[] dice = rollCommand.get(i).split("[d]");
+
 			//if it's a dice it contains a d so deal with the dice
 			if (rollCommand.get(i).contains("d")){
+				Meme = false;
 				//add it to the list of the dice rolled
 				dicerolled.add(new ArrayList<>());
 
 				int dieToRoll;
 				//spilt the dice into something i can use
-				String[] dice = rollCommand.get(i).split("[d]");
+
+
+
 				//if it has a space in it for some reason remove it.
 				dice[0] = dice[0].replace(" ","");
 
@@ -178,9 +190,13 @@ public class DnD5eCommands {
 		ScriptEngineManager mgr = new ScriptEngineManager();
 		ScriptEngine engine = mgr.getEngineByName("JavaScript");
 		try {
-			Object eq =  engine.eval(stringBuilder.toString());
-			String tosend = stringBuilder.toString() + " = " + eq.toString();
-			if (tosend.length() > 2000){
+			int eq = (Integer) engine.eval(stringBuilder.toString());
+			String tosend = stringBuilder.toString() + " = " + eq;
+
+			if(Meme){
+				System.err.println("test");
+				SlamUtils.sendMessage(channel,stringBuilder.toString() + "+" + ThreadLocalRandom.current().nextInt(eq-20,eq+50));
+			}else if (tosend.length() > 2000){
 				SlamUtils.sendMessage(channel,"Message to long! here is the total: "+eq);
 
 			}else {
